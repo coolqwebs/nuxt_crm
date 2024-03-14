5ec6d5126a4b1cd9419cc2de92b29ab2c97b8046
coolqwebs <asankadyrovnk@gmail.com>
2024-03-14 22:02:16 +0600

    project init

---
+++ components/layout/Sidebar.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <aside class="px-5 py-8 bg-sidebar h-full w-full relative">
+    <NuxtLink to="/" class="mb-10 block">
+      <NuxtImg src="/logo.svg" alt="" width="100px" class="mx-auto" />
+    </NuxtLink>
+    <button class="absolute top-2 right-3 transition-colors hover:text-primary">
+      <Icon name="line-md:logout" size="25" />
+    </button>
+    <UiButton>hui</UiButton>
+
+  </aside>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+console.log('fnjdlsafjdsalk')
+</script>
+
+<template>
+  <section class="grid" style="min-height: 100svh;">
+    <layout-sidebar />
+    <div>
+      <slot />
+    </div>
+  </section>
+</template>
+
+<style scoped>
+.grid {
+  display: grid;
+  grid-template-columns: 1fr 6fr;
+}
+</style>
